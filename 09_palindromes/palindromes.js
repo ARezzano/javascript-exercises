2123ae2db3acea63b07823a0f0cb3343fc5ccfdd
Alejandro Rezzano <rezzanoalejandro@gmail.com>
2025-01-28 16:53:57 -0300

    Completed task 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (val) {
+    let normal = val.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reversed = normal.split("").reverse().join(""); 
 
+    if(normal == reversed){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
