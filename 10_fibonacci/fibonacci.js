158505eb1551c5148e05c964bfc04df651a3ecc8
Alejandro Rezzano <rezzanoalejandro@gmail.com>
2025-01-28 17:31:16 -0300

    Complete task 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(val){
+    if(val == 0) return 0;
+    if(val == 1) return 1;
+    if(val < 0) return "OOPS";
 
+    let secondLast = 0;
+    let last = 1;
+    let current = 1;
+
+    for(let i = 2; i < val; i++){
+        secondLast = last;
+        last = current;
+        current = last + secondLast;
+    }
+    return current;
 };
 
 // Do not edit below this line
