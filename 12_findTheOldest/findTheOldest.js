837b8a0fad31a0e0756525623c21d8997770757b
Alejandro Rezzano <rezzanoalejandro@gmail.com>
2025-01-30 00:14:51 -0300

    Complete task 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    if(arr.length === 0) return null;
+    return arr.reduce((acc,curr)=>{
+        const accAge = getAge(acc.yearOfBirth,acc.yearOfDeath);
+        const currAge = getAge(curr.yearOfBirth,curr.yearOfDeath);
 
+        return accAge > currAge ? acc : curr;
+    });
 };
+
+function getAge(birth,death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
